873ab5a316fd0b90862ec847a3e40e6e49d9117d
Shiva23 <124371578+Shivakumar23092001@users.noreply.github.com>
2025-06-02 21:31:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function sayHello() {
+    const userName = prompt("Enter your name:");
+    if (userName) {
+    alert("Hello, " + userName + "!");
+    } else {
+    alert("Hello, guest!");
+    }
+}
